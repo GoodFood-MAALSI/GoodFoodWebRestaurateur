b564672be25a9136c82a557eeac3c4b76dec38eb
unknown <antoine.cuvilliez@outlook.fr>
2025-03-05 14:53:00 +0100

    feat(app) Ajout Dockerfile & port 4002

--- goodfoodwebrestaurateur/next.config.ts
+++ goodfoodwebrestaurateur/next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+  output: "standalone",
+  serverRuntimeConfig: {
+    port: 4002,
+  },
 };
 
 export default nextConfig;
