cdd3335ce1df6fe5ff99a65a9a8bff310d55ff93
LuaScale <luascale@gmail.com>
2025-03-25 00:32:56 +0100

    refactor: updated layout logic and separated client and server layout

---
+++ components/ui/GoodFood/ClientLayout.tsx
@@ -0,0 +1,28 @@
+"use client";
+
+import { usePathname } from "next/navigation";
+import "@/app/globals.css";
+import Footer from "@/components/Footer";
+import Header from "@/components/Header";
+
+export default function ClientLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  const pathname = usePathname();
+  const hideNavbarRoutes = ["/", "/create-company", "/auth"];
+  const showNavbar = !hideNavbarRoutes.includes(pathname);
+
+  return (
+    <>
+      <html lang="fr" className="h-full">
+        <body className="antialiased h-full flex flex-col">
+          <Header showNavbar={showNavbar} />
+          <main className="flex-grow">{children}</main>
+          <Footer />
+        </body>
+      </html>
+    </>
+  );
+}
