083f502330d3b655f3cdace7c2c964c9719425e3
Jules Van Den Eede <luascale@gmail.com>
2025-05-20 22:02:19 +0200

    refactor(restaurant): refactored restaurant logic for cleaner code

---
+++ app/create-company/schema.ts
@@ -0,0 +1,23 @@
+import * as z from "zod";
+
+export const restaurantSchema = z.object({
+  name: z
+    .string()
+    .min(3, "Le nom du restaurant doit comporter au moins 3 caractères"),
+  description: z
+    .string()
+    .min(10, "La description doit comporter au moins 10 caractères"),
+  street_number: z.string().min(1, "Le numéro de rue est requis"),
+  street: z
+    .string()
+    .min(3, "Le nom de la rue doit comporter au moins 3 caractères"),
+  city: z
+    .string()
+    .min(2, "Le nom de la ville doit comporter au moins 2 caractères"),
+  postal_code: z.string().min(4, "Le code postal est requis"),
+  country: z.string().min(2, "Le pays est requis"),
+  email: z.string().email("Adresse email invalide"),
+  phone_number: z.string().regex(/^\d{9,15}$/, "Numéro de téléphone invalide"),
+});
+
+export type RestaurantFormValues = z.infer<typeof restaurantSchema>;
