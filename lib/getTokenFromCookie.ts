938fcb3f3deb856766688234339f3b6723eae384
Jules Van Den Eede <luascale@gmail.com>
2025-06-26 23:29:02 +0200

    feat: lib token for cookie usage

---
+++ lib/getTokenFromCookie.ts
@@ -0,0 +1,7 @@
+import { cookies } from "next/headers";
+
+export async function getAuthToken(): Promise<string | null> {
+  const cookieStore = cookies();
+  const token = (await cookieStore).get("auth_token");
+  return token?.value || null;
+}
