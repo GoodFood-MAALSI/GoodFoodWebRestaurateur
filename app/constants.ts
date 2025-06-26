1dc7d6b3638bc9ef19b30cefcab45e645dfc1855
Jules Van Den Eede <luascale@gmail.com>
2025-06-26 23:30:12 +0200

    refactor: colors handling follow-up

--- app/constants.ts
+++ app/constants.ts
@@ -2,4 +2,8 @@ export const NEXT_PUBLIC_API_URL= "http://localhost:8080/restaurateur/api";
 export const API_ROUTES = {
     restaurants: "http://localhost:8080/restaurateur/api/restaurant",
   };
+  export const colors = {
+    submit: "#76C893",
+    secondary: "#34A0A4",
+  };
   
